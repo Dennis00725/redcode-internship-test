cefc2b2d47fae58a6874521502ff618974cab934
Dennis00725 <dennisdanieldr2001@gmail.com>
2026-07-11 01:06:45 +0200

    feat: add profile and change password endpoints to AuthController

--- obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe6a6126d3f5b7373defba95112d440061ed5893")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cabab5ddcabc4396b267afce46595748eb71982")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
